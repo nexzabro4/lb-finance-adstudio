323bda308fe26602acf8b63caadf8a603d2ed411
nexzabro4 <158283361+nexzabro4@users.noreply.github.com>
2024-01-31 22:53:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Car from './components/car';
-import Weel from './components/weel';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/lb/car" element={<Car />} />
-        <Route path="/lb/weel" element={<Weel />} />
       </Routes>
     </Router>
   );
